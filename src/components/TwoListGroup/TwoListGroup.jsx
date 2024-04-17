f263b844e4a4b5c78e5cdb8b43b891440deaaa98
Christian Nuttli <nuttli.christian@gmail.com>
2024-04-17 17:37:29 +0200

    - fixed moving bog

--- src/components/TwoListGroup/TwoListGroup.jsx
+++ src/components/TwoListGroup/TwoListGroup.jsx
@@ -1,5 +1,5 @@
 import './TwoListGroup.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 
@@ -12,16 +12,21 @@ const [bikesLeft, setBikesLeft] = useState([
     'Triumph Triple Speed',
 ]);
 
-const [bikesRight, setBikesRight] = useState([]);
+const [bikesRight, setBikesRight] = useState([""]);
 const [selectedBike, setSelectedBike] = useState("");
 
+useEffect(() => {
+    setBikesRight([]);
+}, [])
+
 const moveOne = (dir) => {
+    if (!selectedBike) return;
     if (dir === "right" && !bikesRight.includes(selectedBike)) {
-        setBikesRight([...bikesRight, selectedBike]);
+        setBikesRight([selectedBike, ...bikesRight]);
         setBikesLeft(bikesLeft.filter(b => b !== selectedBike));
     }
     else if (dir === "left" && !bikesLeft.includes(selectedBike)) {
-        setBikesLeft([...bikesLeft, selectedBike]);
+        setBikesLeft([selectedBike, ...bikesLeft]);
         setBikesRight(bikesRight.filter(b => b !== selectedBike));
     }
     setSelectedBike("");
@@ -29,11 +34,11 @@ const moveOne = (dir) => {
 
 const moveAll = (dir) => {
     if (dir === "right") {
-        setBikesRight([...bikesRight, bikesLeft]);
+        setBikesRight([...bikesRight, ...bikesLeft]);
         setBikesLeft([]);
     }
     else if (dir === "left") {
-        setBikesLeft([...bikesLeft, bikesRight]);
+        setBikesLeft([...bikesLeft, ...bikesRight]);
         setBikesRight([]);
     }
 }
@@ -55,7 +60,7 @@ return (
 
             <Col md="3">
                 <ListGroup>
-                    {bikesRight.map(bike => <ListGroup.Item key={bike}>{bike}</ListGroup.Item>)}
+                {bikesRight.map(bike => <ListGroup.Item active={selectedBike === bike} action onClick={() => setSelectedBike(bike)} key={bike}>{bike}</ListGroup.Item>)}
                 </ListGroup>
             </Col>
         </Row>
