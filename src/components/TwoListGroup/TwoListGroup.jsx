da92396c2ee8a98e64eb3fe08a8d0cb43246e0e8
Christian Nuttli <nuttli.christian@gmail.com>
2024-04-17 18:01:35 +0200

    - implemented multiselect and deselect on both sides

--- src/components/TwoListGroup/TwoListGroup.jsx
+++ src/components/TwoListGroup/TwoListGroup.jsx
@@ -4,67 +4,96 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 
 export default function TwoListGroup() {
-const [bikesLeft, setBikesLeft] = useState([
-    'Honda CBR 1000',
-    'Ducati Monster 1200s',
-    'Yamaha MT-01',
-    'KTM Superduke 1200',
-    'Triumph Triple Speed',
-]);
+    const [bikesLeft, setBikesLeft] = useState([
+        'Honda CBR 1000',
+        'Ducati Monster 1200s',
+        'Yamaha MT-01',
+        'KTM Superduke 1200',
+        'Triumph Triple Speed',
+    ]);
 
-const [bikesRight, setBikesRight] = useState([""]);
-const [selectedBike, setSelectedBike] = useState("");
+    const [bikesRight, setBikesRight] = useState([""]);
+    const [selectedBikesLeft, setSelectedBikesLeft] = useState([""]);
+    const [selectedBikesRight, setSelectedBikesRight] = useState([""]);
 
-useEffect(() => {
-    setBikesRight([]);
-}, [])
-
-const moveOne = (dir) => {
-    if (!selectedBike) return;
-    if (dir === "right" && !bikesRight.includes(selectedBike)) {
-        setBikesRight([selectedBike, ...bikesRight]);
-        setBikesLeft(bikesLeft.filter(b => b !== selectedBike));
-    }
-    else if (dir === "left" && !bikesLeft.includes(selectedBike)) {
-        setBikesLeft([selectedBike, ...bikesLeft]);
-        setBikesRight(bikesRight.filter(b => b !== selectedBike));
-    }
-    setSelectedBike("");
-}
-
-const moveAll = (dir) => {
-    if (dir === "right") {
-        setBikesRight([...bikesRight, ...bikesLeft]);
-        setBikesLeft([]);
-    }
-    else if (dir === "left") {
-        setBikesLeft([...bikesLeft, ...bikesRight]);
+    useEffect(() => {
         setBikesRight([]);
+        setSelectedBikesLeft([]);
+        setSelectedBikesRight([]);
+    }, [])
+
+    const clearSelections = () => {
+        setSelectedBikesLeft([]);
+        setSelectedBikesRight([]);
     }
-    setSelectedBike("");
-}
 
-return (
-    <Container>
-        <Row>
-            <Col md="3">
-                <ListGroup>
-                    {bikesLeft.map(bike => <ListGroup.Item active={selectedBike === bike} action onClick={() => setSelectedBike(bike)} key={bike}>{bike}</ListGroup.Item>)}
-                </ListGroup>
-            </Col>
-            <Col md="auto" className='btn-group-vertical'>
-                <Button action onClick={() => moveAll("right")}>{">>"}</Button>
-                <Button action onClick={() => moveOne("right")}>{">"}</Button>
-                <Button action onClick={() => moveOne("left")}>{"<"}</Button>
-                <Button action onClick={() => moveAll("left")}>{"<<"}</Button>
-            </Col>
+    const moveBikes = (dir) => {
+        if (dir === "right" && selectedBikesLeft.length > 0) {
+            setBikesRight([...selectedBikesLeft, ...bikesRight]);
+            setBikesLeft(bikesLeft.filter(b => !selectedBikesLeft.includes(b)));
+            clearSelections();
+        }
+        else if (dir === "left" && selectedBikesRight.length > 0) {
+            setBikesLeft([...selectedBikesRight, ...bikesLeft]);
+            setBikesRight(bikesRight.filter(b => !selectedBikesRight.includes(b)));
+            clearSelections();
+        }
+    }
 
-            <Col md="3">
-                <ListGroup>
-                    {bikesRight.map(bike => <ListGroup.Item active={selectedBike === bike} action onClick={() => setSelectedBike(bike)} key={bike}>{bike}</ListGroup.Item>)}
-                </ListGroup>
-            </Col>
-        </Row>
-    </Container>
-);
+    const moveAll = (dir) => {
+        if (dir === "right") {
+            setBikesRight([...bikesRight, ...bikesLeft]);
+            setBikesLeft([]);
+        }
+        else if (dir === "left") {
+            setBikesLeft([...bikesLeft, ...bikesRight]);
+            setBikesRight([]);
+        }
+        clearSelections();
+    }
+
+    const bikeSelectionHandler = (bike, dir) => {
+        if (dir === "left") {
+            if (selectedBikesLeft.includes(bike)) {
+                setSelectedBikesLeft([...selectedBikesLeft.filter(b => b !== bike)]);
+            }
+            else {
+                setSelectedBikesLeft([bike, ...selectedBikesLeft]);
+            }
+            setSelectedBikesRight([]);
+        }
+        else if (dir === "right") {
+            if (selectedBikesRight.includes(bike)) {
+                setSelectedBikesRight([...selectedBikesRight.filter(b => b !== bike)]);
+            }
+            else {
+                setSelectedBikesRight([bike, ...selectedBikesRight]);
+            }
+            setSelectedBikesLeft([]);
+        }
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Col md="3">
+                    <ListGroup>
+                        {bikesLeft.map(bike => <ListGroup.Item active={selectedBikesLeft.includes(bike)} action onClick={() => bikeSelectionHandler(bike, "left")} key={bike}>{bike}</ListGroup.Item>)}
+                    </ListGroup>
+                </Col>
+                <Col md="auto" className='btn-group-vertical'>
+                    <Button action onClick={() => moveAll("right")}>{">>"}</Button>
+                    <Button action onClick={() => moveBikes("right")}>{">"}</Button>
+                    <Button action onClick={() => moveBikes("left")}>{"<"}</Button>
+                    <Button action onClick={() => moveAll("left")}>{"<<"}</Button>
+                </Col>
+
+                <Col md="3">
+                    <ListGroup>
+                        {bikesRight.map(bike => <ListGroup.Item active={selectedBikesRight.includes(bike)} action onClick={() => bikeSelectionHandler(bike, "right")} key={bike}>{bike}</ListGroup.Item>)}
+                    </ListGroup>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
